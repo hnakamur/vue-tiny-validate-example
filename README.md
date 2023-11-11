0ac5789023ce619175909db014d9f742d3ee2589
Hiroaki Nakamura <hnakamur@gmail.com>
2023-11-11 11:51:02 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# vue-tiny-validate-example
+
+This is my example for [Validate array of objects · Issue #27 · FrontLabsOfficial/vue-tiny-validate](https://github.com/FrontLabsOfficial/vue-tiny-validate/issues/27).
+When you click Add or Delete button, rules are updated dynamically.
+
+## Project Setup
+
+```sh
+npm install
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm run dev
+```
+
+### Type-Check, Compile and Minify for Production
+
+```sh
+npm run build
+```
+
+### Lint with [ESLint](https://eslint.org/)
+
+```sh
+npm run lint
+```
---
+++ src/App.vue
@@ -0,0 +1,69 @@
+<script setup lang="ts">
+import { reactive, ref, computed } from 'vue'
+import useValidate from 'vue-tiny-validate'
+
+const initialData = { lhs: '', rhs: '' }
+
+const data = reactive({
+  equations: [structuredClone(initialData)]
+})
+
+const testSumToBe9 = (value: any) => {
+  const lhs = value.lhs
+  const rhs = value.rhs
+  const sum = Number(lhs) + Number(rhs)
+  const result = sum === 9
+  return result
+}
+
+const ruleSumMustBe9 = {
+  name: 'sumMustBe9',
+  test: testSumToBe9,
+  message: 'please input two numbers whose sum is 9'
+}
+
+const rules = reactive({
+  equations: {
+    0: ruleSumMustBe9
+  }
+})
+const options = reactive({ autoTouch: true })
+
+const { result } = useValidate(data, rules, options)
+
+const inputted = ref(false)
+const handleInput = () => {
+  result.value.$test()
+  inputted.value = true
+}
+
+const message = computed(() =>
+  inputted.value
+    ? result.value.$invalid
+      ? 'Some equation(s) are not correct'
+      : 'All equations are correct!'
+    : 'Input pairs of numbers whose sum is 9.'
+)
+
+const handleAddClick = (index: number) => {
+  (rules.equations as any)[String(data.equations.length)] = ruleSumMustBe9
+  data.equations.splice(index + 1, 0, structuredClone(initialData))
+}
+
+const handleDeleteClick = (index: number) => {
+  data.equations.splice(index + 1, 1)
+  delete (rules.equations as any)[String(data.equations.length)]
+}
+</script>
+
+<template>
+  <div>{{ message }}</div>
+  <template v-for="(equation, index) in data.equations" v-bind:key="index">
+    <div>
+      <input v-model="equation.lhs" type="numeric" min="1" v-on:input="handleInput" /> +
+      <input v-model="equation.rhs" type="numeric" min="1" v-on:input="handleInput" /> = 9
+      <input type="button" value="Add" v-on:click="handleAddClick(index)" />
+      <input type="button" value="Delete" v-on:click="handleDeleteClick(index)" />
+    </div>
+  </template>
+</template>
